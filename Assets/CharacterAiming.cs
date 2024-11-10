8daaff5495cd9e84a0836afdda377f52c05280ef
yubinx <yubinxie222@gmail.com>
2024-11-10 09:17:57 -0500

    Removed Dampening for Camera that altered strafing direction

---
+++ Assets/CharacterAiming.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CharacterAiming : MonoBehaviour
+{
+    public float turningSpeed = 15f;
+    Camera mainCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, yawCamera, 0f), turningSpeed * Time.fixedDeltaTime);
+    }
+}
