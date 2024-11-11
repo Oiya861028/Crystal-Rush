3aaa56a18ab8f65c85c1863544410c20752dbb00
yubinx <yubinxie222@gmail.com>
2024-11-11 10:29:38 -0500

    Add Bullet Trail to Gun

--- Assets/RaycastWeapon.cs
+++ Assets/RaycastWeapon.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class RaycastWeapon : MonoBehaviour
 {
     public ParticleSystem muzzleFlash;
+    public ParticleSystem hitEffect;
+    public TrailRenderer TraceEffect;
     public Transform RaycastOrigin;
     public Transform RaycastDestination;
     public bool isFiring = false;
+    public float fireRate = .05f;
+    float timeSincegunFired;
     Ray ray;
     RaycastHit hitInfo;
     public void StartFiring() {
-        ray.origin = RaycastOrigin.position;
-        ray.direction = RaycastDestination.position - RaycastOrigin.position;
-        if(Physics.Raycast(ray, out hitInfo)) {
-            Debug.DrawLine(ray.origin, hitInfo.point, Color.red, 1f);
+        if(timeSincegunFired>=fireRate){
+            ray.origin = RaycastOrigin.position;
+            ray.direction = RaycastDestination.position - RaycastOrigin.position;
+            var tracer = Instantiate(TraceEffect, ray.origin, Quaternion.identity);
+            tracer.AddPosition(ray.origin);
+            if(Physics.Raycast(ray, out hitInfo)) {
+                //Debug.DrawLine(ray.origin, hitInfo.point, Color.red, 1f);
+                hitEffect.transform.position = hitInfo.point;
+                hitEffect.transform.forward = hitInfo.normal;
+                hitEffect.Emit(1);
+                tracer.transform.position = hitInfo.point;
+            }
+            timeSincegunFired=0;
+            isFiring = true;
+            muzzleFlash.Emit(1);
+        }
+        else{
+            timeSincegunFired+=Time.deltaTime;
         }
 
-        isFiring = true;
-        muzzleFlash.Emit(1);
+        
     }
     public void StopFiring() {
         isFiring = false;
