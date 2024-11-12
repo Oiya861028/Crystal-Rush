ff1fe6d0dc409949251516797616df92f00421e9
yubinx <yubinxie222@gmail.com>
2024-11-11 21:27:19 -0500

    Basic Health and Damage Script

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -8,14 +8,20 @@ public class Health : MonoBehaviour
     public float currentHealth;
     void Start() {
         currentHealth = MaxHealth;
+        var rigidbodies = GetComponentsInChildren<Rigidbody>();
+        foreach(var rigidbody in rigidbodies){
+            HitBox hitbox = rigidbody.gameObject.AddComponent<HitBox>();
+            hitbox.health = this;
+        }
     }
-    public void TakeDamage(float amount) {
+    public void TakeDamage(float amount, Vector3 direction) {
         currentHealth -= amount;
         if(currentHealth <= 0.0f) {
             die();
         }
     } 
     private void die() {
-        
+        Debug.Log("DEAD");
+        Destroy(this);
     }
 }
