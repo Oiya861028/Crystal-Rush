4d5b043a87299f67c07a5c9210b492612cc61c4e
yubinx <yubinxie222@gmail.com>
2024-11-11 23:02:43 -0500

    Fixed Not being able to collect Crystal

--- Assets/Script/CollectCrystal.cs
+++ Assets/Script/CollectCrystal.cs
@@ -3,17 +3,29 @@ using UnityEngine;
 public class CollectCrystal : MonoBehaviour
 {
     public int points = 1; // Points to add when collected
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            // GameManager gameManager = FindObjectOfType<GameManager>(); // Find the GameManager
-            // if (gameManager != null)
-            // {
-            //     gameManager.AddScore(points); // Add points to the score
-            // }
-            DestroyImmediate(gameObject); // Remove the treasure
+    private Transform playerTransform;
+    public float detectRange = 1.0f;
+    void Start() {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update(){
+        float distance = Vector3.Distance(playerTransform.position, transform.position);
+        if(distance<detectRange){
+            Debug.Log("Destroyed");
+            DestroyImmediate(gameObject);
         }
     }
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.CompareTag("Player"))
+    //     {
+    //         // GameManager gameManager = FindObjectOfType<GameManager>(); // Find the GameManager
+    //         // if (gameManager != null)
+    //         // {
+    //         //     gameManager.AddScore(points); // Add points to the score
+    //         // }
+    //          // Remove the treasure
+    //     }
+    // }
 }
