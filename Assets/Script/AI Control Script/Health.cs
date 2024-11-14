d19b36e6cb414c630057121830e44e19a6da7845
yubinx <yubinxie222@gmail.com>
2024-11-14 13:05:15 -0500

    We using this health script instead

--- Assets/Script/AI Control Script/Health.cs
+++ Assets/Script/AI Control Script/Health.cs
@@ -8,22 +8,25 @@ public class Health : MonoBehaviour
     [HideInInspector]
     public float currentHealth;
     void Start() {
-        agent = GtetComponent<AIagent>();
+        agent = GetComponent<AIAgent>();
         currentHealth = MaxHealth;
         var rigidbodies = GetComponentsInChildren<Rigidbody>();
-        foreach(var rigidbody in rigidbodies){
+        foreach(var rigidbody in rigidbodies) {
             HitBox hitbox = rigidbody.gameObject.AddComponent<HitBox>();
             hitbox.health = this;
         }
     }
     public void TakeDamage(float amount) {
         currentHealth -= amount;
+        if(currentHealth <= 0.3*MaxHealth) {
+            agent.stateMachine.ChangeState(AIStateId.Flee);
+        }
         if(currentHealth <= 0.0f) {
             die();
         }
     } 
     private void die() {
-        AIDeathState deathState = agent.StateMachine.GetState(AIStateId.Death) as AIDeathState;
-        agent.stateMachine.ChangeState(AIStateId.Death);
+        AIDeathState deathState = agent.stateMachine.GetState(AIStateId.Die) as AIDeathState;
+        agent.stateMachine.ChangeState(AIStateId.Die);
     }
 }
